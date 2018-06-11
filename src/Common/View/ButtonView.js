783d39e15554d5f36c19ca5deea20e2e78f3ecf7
mason <mason-jx@jxinternet.com>
2018-06-11 15:36:55 +0800

    optimize|还原自定义ButtonView的间距

--- src/Common/View/ButtonView.js
+++ src/Common/View/ButtonView.js
@@ -75,7 +75,7 @@ const styles = StyleSheet.create({
         color: 'white',
         textAlign: 'center',
         textAlignVertical: 'center',
-        // padding: 8,
+        padding: 8,
         fontWeight: '500',
     },
     textDisabled: {
