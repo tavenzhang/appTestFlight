7afffe98df6843d281a685666ad527a1479065be
mason <mason-jx@jxinternet.com>
2018-09-18 17:23:36 +0800

    [修复]1、修复乐游平台投机记录详情页面加载失败的问题。

--- src/Page/UserCenter/UserOrder/TCUserFGBetDetail.js
+++ src/Page/UserCenter/UserOrder/TCUserFGBetDetail.js
@@ -4,7 +4,7 @@
  * Created by Allen on 2016/12/10.
  */
 import React, {Component} from 'react';
-import {ListView, View} from 'react-native';
+import {View} from 'react-native';
 import TopNavigationBar from '../../../Common/View/TCNavigationBar';
 import NavigatorHelper from "../../../Common/JXHelper/TCNavigatorHelper";
 import {indexBgColor} from '../../resouce/theme'
@@ -12,26 +12,23 @@ import BetOrderItemComponent from "./BetOrderItemComponent";
 
 export default class TCUserFGBetDetail extends Component {
 
-    constructor(props) {
-        super(props)
-        this.orderData = this.props.orderData
-    }
-
     render() {
-        if (this.orderData.gt === 'fish') {
+        let params = this.props.navigation.state.params;
+        let {orderData} = params
+        if (orderData.gt === 'fish') {
             return (
                 <View style={{flex: 1, backgroundColor: indexBgColor.mainBg}}>
                     <TopNavigationBar
                         title={'投注详情'}
                         needBackButton
                         backButtonCall={() => NavigatorHelper.popToBack()}/>
-                    <BetOrderItemComponent title={'游戏名称'} content={this.orderData.fishDetail.gameName} />
-                    <BetOrderItemComponent title={'游戏类目名称'} content={this.orderData.fishDetail.categoryName} />
-                    <BetOrderItemComponent title={'订单号'} content={this.orderData.fishDetail.transactionId} />
-                    <BetOrderItemComponent title={'投注金额'} content={this.orderData.totalWager} />
-                    <BetOrderItemComponent title={'输赢'} content={this.orderData.playerWinLoss} />
-                    <BetOrderItemComponent title={'游戏开始时间'} content={this.orderData.fishDetail.startTime.replace('T', ' ')} />
-                    <BetOrderItemComponent title={'游戏结束时间'} content={this.orderData.fishDetail.endTime.replace('T', ' ')} />
+                    <BetOrderItemComponent title={'游戏名称'} content={orderData.fishDetail.gameName} />
+                    <BetOrderItemComponent title={'游戏类目名称'} content={orderData.fishDetail.categoryName} />
+                    <BetOrderItemComponent title={'订单号'} content={orderData.fishDetail.transactionId} />
+                    <BetOrderItemComponent title={'投注金额'} content={orderData.totalWager} />
+                    <BetOrderItemComponent title={'输赢'} content={orderData.playerWinLoss} />
+                    <BetOrderItemComponent title={'游戏开始时间'} content={orderData.fishDetail.startTime.replace('T', ' ')} />
+                    <BetOrderItemComponent title={'游戏结束时间'} content={orderData.fishDetail.endTime.replace('T', ' ')} />
                 </View>
             )
         } else {
@@ -41,12 +38,12 @@ export default class TCUserFGBetDetail extends Component {
                         title={'投注详情'}
                         needBackButton
                         backButtonCall={() => NavigatorHelper.popToBack()}/>
-                    <BetOrderItemComponent title={'游戏名称'} content={this.orderData.detail.gameName} />
-                    <BetOrderItemComponent title={'游戏类目名称'} content={this.orderData.detail.categoryName} />
-                    <BetOrderItemComponent title={'订单号'} content={this.orderData.detail.transactionId} />
-                    <BetOrderItemComponent title={'投注金额'} content={this.orderData.totalWager} />
-                    <BetOrderItemComponent title={'输赢'} content={this.orderData.playerWinLoss} />
-                    <BetOrderItemComponent title={'时间'} content={this.orderData.detail.time.replace('T', ' ')} />
+                    <BetOrderItemComponent title={'游戏名称'} content={orderData.detail.gameName} />
+                    <BetOrderItemComponent title={'游戏类目名称'} content={orderData.detail.categoryName} />
+                    <BetOrderItemComponent title={'订单号'} content={orderData.detail.transactionId} />
+                    <BetOrderItemComponent title={'投注金额'} content={orderData.totalWager} />
+                    <BetOrderItemComponent title={'输赢'} content={orderData.playerWinLoss} />
+                    <BetOrderItemComponent title={'时间'} content={orderData.detail.time.replace('T', ' ')} />
                 </View>
             )
         }
