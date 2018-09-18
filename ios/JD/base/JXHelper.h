9b51b99c8e5597619ab3f45a54a28bd39d237c66
Sam <sam-jx@jxinternet.com>
2018-09-18 14:46:20 +0800

    [功能]：1、易盈棋牌更换logo。

---
+++ ios/JD/base/JXHelper.h
@@ -0,0 +1,14 @@
+//
+//  JXHelper.h
+//  JD-Lottery
+//
+//  Created by Sam on 2018/9/18.
+//  Copyright © 2018年 JD. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <React/RCTBridgeModule.h>
+
+@interface JXHelper : NSObject<RCTBridgeModule>
+
+@end
