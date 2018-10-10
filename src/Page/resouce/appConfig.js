9d852b6f33c5edc4c5205b9559dcd1b0946678d0
taven-jx <taven-jx@jxinternet.com>
2018-10-10 14:40:35 +0800

    autoMerge develop
    # Conflicts:
    #	src/Page/resouce/images.js

--- src/Page/resouce/appConfig.js
+++ src/Page/resouce/appConfig.js
@@ -15,26 +15,26 @@ import React, {
 
 
 export const appDomainBase = {
-    base1: 'https://www.kpi002.com',
-    base2: 'https://www.12shkhflkaaa.com',
-    base3: 'https://www.12kdrskdfjccb.com',
-    base4: 'https://www.12kckkfjdkjffs.com',
-    base5: 'https://www.12jjj4hdjaxxfc.com',
-    base6: 'https://www.12kdkjfjrccc.com',
-    base7: 'https://www.12kfkjtrcmdjs.com',
+    base1: 'https://www.gou6000.com',
+    base2: 'https://www.12derfcgvtw.com',
+    base3: 'https://www.12kkdfjmnd.com',
+    base4: 'https://www.12prmkjdsew.com',
+    base5: 'https://www.12iiikpoadsf.com',
+    base6: 'https://www.12frtgvbnmk.com',
+    base7: 'https://www.12jfhtyklmd.com',
 }
 
 export const configAppId = '1'
-    // export const appDomainBase = {
-    //     base1: 'http://192.168.12.25:7500',
-    //     base2: 'http://192.168.12.23111:7500',
-    //     base3: 'http://192.168.12.2411:7500',
-    //     base4: 'http://192.168.1.93:7500',
-    //     base5: 'http://192.168.12.2111:7500',
-    //     base6: 'http://192.168.12.22311:7500',
-    //     base7: 'http://192.168.12.21211:7500',
-    // }
-    // export const configAppId = '31'
+// export const appDomainBase = {
+//     base1: 'http://192.168.12.25:7500',
+//     base2: 'http://192.168.12.23111:7500',
+//     base3: 'http://192.168.12.2411:7500',
+//     base4: 'http://192.168.1.93:7500',
+//     base5: 'http://192.168.12.2111:7500',
+//     base6: 'http://192.168.12.22311:7500',
+//     base7: 'http://192.168.12.21211:7500',
+// }
+// export const configAppId = '31'
 
 export const trendServer = 'http://106caipiao.com'
 export const MyAppName = '易盈棋牌'
