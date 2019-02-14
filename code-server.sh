44792cef7728b751abc044c8c71627cc83adee6d
Taven <taven.zhang@pactmail.com>
2019-02-14 16:54:32 +0800

    init

---
+++ code-server.sh
@@ -0,0 +1,40 @@
+code-push login http://192.168.14.70:3000 --key 5qzQb36e81sCYuBgboVYRmgbs1of4ksvOXqog
+code-push release bbl-ios  ./ios/bundle  1.1.1 
+code-push release bbl-android  ./android/bundle 1.1.1 --description 'app优化,请点确定自动更新！'
+
+
+code-push release-react bbl-ios ios -t 1.1.1  --description 'app init22'
+
+code-push release-react bbl-ios ios  -t 1.1.1 --d Staging --description 'app init2231'
+
+code-push login https://checkupdate5.v5maomao.com --key e2EPdAzBmMVOmzi88wCFAOyMHMKX4ksvOXqog
+ "url": "https://checkupdate5.v5maomao.com",         "token": "e2EPdAzBmMVOmzi88wCFAOyMHMKX4ksvOXqog"
+
+code-push login https://checkupdate5.v5maomao.com --key  e2EPdAzBmMVOmzi88wCFAOyMHMKX4ksvOXqog
+
+code-push release-react uat01CGgame-ios ios -t 1.0.0  --d Production --description 'app uat'
+
+code-push release-react uat01CGgame-android android -t 1.0.0 --d Production --description 'app new333'
+
+code-push release-react uat01CGgame-android android -t 1.0.2 --d Production --description 'app new1_9'
+
+code-push rollback uat01CGgame-android Production --targetRelease  v29
+
+
+https://checkupdate5.v5maomao.com
+code-push login "https://checkupdate5.v5maomao.com" --key  e2EPdAzBmMVOmzi88wCFAOyMHMKX4ksvOXqog
+
+https://download.d8-app.com/ios/bbl/game.json
+https://download.jwyxw.net/ios/bbl/game.json
+
+
+
+code-push login "https://cg-checkupdate34.qingjian8.com" --key  4UImTKoVW6hSE4aD3r1R8a9071jP4ksvOXqog
+code-push release-react 365CG-android android -t 2.2.2 --d Production --description '365-android'
+code-push release-react 365CG-ios ios -t 2.2.2 --d Production --description '365-ios'
+
+https://download.weihaizaixian.com/game/app.html?sub=uat-ios&&app=1
+https://download.weihaizaixian.com/game/uat/android/uat_release.apk
+
+热更新域名： cg-checkupdate34.qingjian8.com
+热更新TOKEN: 4UImTKoVW6hSE4aD3r1R8a9071jP4ksvOXqog
