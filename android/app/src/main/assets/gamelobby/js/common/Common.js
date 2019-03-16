bda52ab8b64fe30f13f7f85bb1699f82bbd07dd9
taven <abcd1234>
2019-03-16 16:41:11 +0800

    【bug】临时保持

--- android/app/src/main/assets/gamelobby/js/common/Common.js
+++ android/app/src/main/assets/gamelobby/js/common/Common.js
@@ -151,7 +151,7 @@ var Common = /** @class */ (function () {
     // public static GM_SCREEN_W:number = 1136;
     // public static IS_NATIVE_APP:boolean = false;
     Common.GM_SCREEN_H = 750;
-    Common.GM_SCREEN_W = 1334;
+    Common.GM_SCREEN_W = 1624;
     //loading及toast的深度
     Common.IDX_TOP_LOADING = 9999;
     Common.IDX_TOP_TOAST = 99999;
