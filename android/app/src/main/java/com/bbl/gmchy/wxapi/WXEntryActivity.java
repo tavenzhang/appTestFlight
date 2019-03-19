978bdfe80e29ceb305ce77c408ef539a767dab47
Jaz <jaz.low@pactmail.com>
2019-03-19 11:03:40 +0800

    还原包名，过后打包时以脚本替换包名不需提交

--- android/app/src/main/java/com/bbl/gmchy/wxapi/WXEntryActivity.java
+++
@@ -1,9 +0,0 @@
-package com.bbl.gmchy.wxapi;
-
-import com.umeng.socialize.weixin.view.WXCallbackActivity;
-
-/**
- * Created by wangfei on 17/8/28.
- */
-
-public class WXEntryActivity extends WXCallbackActivity {}
---
+++ android/app/src/main/java/com/jd/audio/RNAudioModule.java
@@ -0,0 +1,43 @@
+package com.jd.audio;
+
+import android.media.AudioManager;
+import android.media.SoundPool;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.jd.R;
+
+/**
+ * @author: Mason
+ */
+public class RNAudioModule extends ReactContextBaseJavaModule {
+
+    private SoundPool mSoundPool;
+    private int mSoundId;
+
+    public RNAudioModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        //mSoundPool = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+       // mSoundId = mSoundPool.load(reactContext, R.raw.bet, 1);
+    }
+
+    @Override
+    public String getName() {
+        return "RNAudio";
+    }
+
+    @ReactMethod
+    public void playBet() {
+        if (null != mSoundPool) {
+            mSoundPool.play(mSoundId, 1, 1, 0, 0, 1);
+        }
+    }
+
+    @ReactMethod
+    public void release() {
+        if (null != mSoundPool) {
+            mSoundPool.release();
+        }
+    }
+}
