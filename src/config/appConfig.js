1f18de49792c1cc751a4050ca9b8513473902406
taven <abcd1234>
2019-03-28 11:27:19 +0800

    【功能】博必胜第一版

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -3,17 +3,16 @@ import React, {
 } from 'react-native';
 
 export const appDomainBase = {
-    base1: 'https://www.diwudalu.com',
-    base2: 'https://www.diwudalu.com',
-    base3: 'https://www.diwudalu.com',
-    base4: 'https://www.diwudalu.com',
-    base5: 'https://www.diwudalu.com',
-    base6: 'https://www.diwudalu.com',
-    base7: 'https://www.diwudalu.com',
+    base1: 'https://38fo28gk.czdelong.com',
+    base2: 'https://38fo28gk.fdk800.com',
+    base3: 'https://38fo28gk.jdzdingli.com',
+    base4: 'https://hosobw29.czdelong.com',
+    base5: 'https://hosobw29.jdzdingli.com',
+    base6: 'https://hosobw29.fdk800.com',
+    base7: 'https://www.vdfrtw654qefhj.com',
 }
 
-
-export let configAppId = "5"
+export let configAppId = "1146"
 
 
 export const AppConfig = {
@@ -27,48 +26,38 @@ export const AppConfig = {
         appDomainBase.base4,
         appDomainBase.base5,
         appDomainBase.base6
-    ],
-    checkUpdateDomains:[
-        "https://www.ba2d16.com",
-        "https://www.aa2d16.com",
-        "https://www.ca2d16.com"
-    ],
+    ]
 }
 
 
-export const MyAppName = '博博乐';
+export const MyAppName = '梦想游戏';
 
-export const versionHotFix = 'bbl_3_25';
-
-export const MyOwnerPlatName= 'uat彩票';
+export const versionHotFix = 'mxqp_3_25';
 
 export const safeguardDomain = [
-    'https://987645ba00a9b0416b254f33d918ed64.oss-cn-shenzhen.aliyuncs.com',
-    'https://c40b6e3d664556ab423d3eebc01ab2fd.oss-cn-shenzhen.aliyuncs.com']
+    "https://37a9aea1be8915f4998443873ec90db0.oss-cn-shenzhen.aliyuncs.com",
+    "https://37a9aea1be8915f4998443873ec90db0.s3-accelerate.amazonaws.com",
+    "https://37a9aea1be8915f4998443873ec90db0.azureedge.net"]
 
 
 export const platInfo = {
-    //loginDomain:"https://0365cai.cc",
-    loginDomain: "https://webplatform.psxiaohe.com",
-    //  gameDomain:"https://dk3nuq7v.zhdisi.com",
-     gameDomain: "https://webplatform.psxiaohe.com",
+    loginDomain:"https://38fo28gk.czdelong.com",
+    gameDomain:"https://yw8txj2e.pgzlwx.com",
 
     zipCheckServer: {
         debug_server: "http://192.168.14.70:8888",
-        release_server: "https://download.hkbaoxian188.com/game/release/uat"
+        release_server: "https://download.hkbaoxian188.com/game/release/mxaw7"
     },
     platId: configAppId,
-    brand: "uat01",
+    brand: "mxqp",
     channel: {}
 }
 
-
 export const affCodeList = {
     ios: {
-        '2.9.6': 'uat_ios'
+        '2.9.6': 'mxaw7_ios'
     },
     android: {
-        '2.10.11': 'uat_android'
+        '2.10.11': 'mxaw7_android'
     }
 };
-
