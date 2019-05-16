2a444554d71f17083260550704c0f695ce36991e
taven <abcd1234>
2019-05-16 20:28:25 +0800

    【功能】优化加载

--- src/Page/WebView/LoadingWebView.js
+++ src/Page/WebView/LoadingWebView.js
@@ -44,9 +44,9 @@ export default class LoadingWebView extends Component {
     render() {
         let newUrl = TW_Store.dataStore.getHomeWebHome() + "/loading/loading.html";
         let myParam = "";
-        // if(TW_Store.dataStore.isAppUnZip) {
-        //
-        // }
+        if(TW_Store.dataStore.isAppUnZip) {
+
+        }
         let source = {
             file: newUrl,
             allowingReadAccessToURL: TW_Store.dataStore.getGameRootDir(),
