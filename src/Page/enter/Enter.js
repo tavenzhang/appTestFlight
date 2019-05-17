f8db7b154e38fe8cb120e54b4a2475b5281e5d14
taven <abcd1234>
2019-05-17 10:23:08 +0800

    uat 更新调试

--- src/Page/enter/Enter.js
+++ src/Page/enter/Enter.js
@@ -23,7 +23,7 @@ import TopNavigationBar from '../../Common/View/TCNavigationBar';
 import {width, Size} from '../asset/game/themeComponet'
 import StartUpHelper from './StartUpHelper'
 import KeepAwake from 'react-native-keep-awake';
-import ExtraDimensions from 'react-native-extra-dimensions-android';
+//import ExtraDimensions from 'react-native-extra-dimensions-android';
 
 let retryTimes = 0
 let downloadTime = 0
@@ -52,18 +52,18 @@ export default class Enter extends Component {
             KeepAwake.activate();
         }
         //如果是android 在某些机器获取不到真实的SCREEN_H SCREEN_W 需要如下处理
-        if (ExtraDimensions && ExtraDimensions.getRealWindowHeight) {
-            // TW_Log("ExtraDimensions--getRealWindowHeight--"  + ExtraDimensions.getRealWindowHeight(),SCREEN_H)
-            // TW_Log("ExtraDimensions--getRealWindowWidth--"  + ExtraDimensions.getRealWindowWidth(),SCREEN_W)
-            // TW_Log("ExtraDimensions--getSoftMenuBarHeight--"  + ExtraDimensions.getSoftMenuBarHeight())
-            // TW_Log("ExtraDimensions--getSmartBarHeight--"  + ExtraDimensions.getSmartBarHeight())
-            // TW_Log("ExtraDimensions--isSoftMenuBarEnabled--"  + ExtraDimensions.isSoftMenuBarEnabled())
-            let rH = ExtraDimensions.getRealWindowHeight();
-            let rW = ExtraDimensions.getRealWindowWidth();
-            JX_PLAT_INFO.SCREEN_H= SCREEN_H = rH && rH > 0 ? rH : SCREEN_H;
-            JX_PLAT_INFO.SCREEN_W= SCREEN_W = rW && rW > 0 ? rW : SCREEN_W;
-            TW_Store.appStore.screenW=rW;
-        }
+        // if (ExtraDimensions && ExtraDimensions.getRealWindowHeight) {
+        //     // TW_Log("ExtraDimensions--getRealWindowHeight--"  + ExtraDimensions.getRealWindowHeight(),SCREEN_H)
+        //     // TW_Log("ExtraDimensions--getRealWindowWidth--"  + ExtraDimensions.getRealWindowWidth(),SCREEN_W)
+        //     // TW_Log("ExtraDimensions--getSoftMenuBarHeight--"  + ExtraDimensions.getSoftMenuBarHeight())
+        //     // TW_Log("ExtraDimensions--getSmartBarHeight--"  + ExtraDimensions.getSmartBarHeight())
+        //     // TW_Log("ExtraDimensions--isSoftMenuBarEnabled--"  + ExtraDimensions.isSoftMenuBarEnabled())
+        //     let rH = ExtraDimensions.getRealWindowHeight();
+        //     let rW = ExtraDimensions.getRealWindowWidth();
+        //     JX_PLAT_INFO.SCREEN_H= SCREEN_H = rH && rH > 0 ? rH : SCREEN_H;
+        //     JX_PLAT_INFO.SCREEN_W= SCREEN_W = rW && rW > 0 ? rW : SCREEN_W;
+        //     TW_Store.appStore.screenW=rW;
+        // }
         AppState.addEventListener('change',this._handleAppStateChange);
     }
 
