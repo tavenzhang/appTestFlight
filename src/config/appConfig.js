ce58945eab8cbd8850bb3ac90204a9aed4b45322
taven <abcd1234>
2019-05-27 16:29:42 +0800

    [bug]新平台优化

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -3,17 +3,17 @@ import React, {
 } from 'react-native';
 
 export const appDomainBase = {
-    base1: 'https://www.diwudalu.com',
-    base2: 'https://www.diwudalu.com',
-    base3: 'https://www.diwudalu.com',
-    base4: 'https://www.diwudalu.com',
-    base5: 'https://www.diwudalu.com',
-    base6: 'https://www.diwudalu.com',
-    base7: 'https://www.diwudalu.com',
+    base1: 'https://1cituv188qp.8vo79h.com',
+    base2: 'https://pb4wxx188qp.8vo79h.com',
+    base3: 'https://5ugcqu188qp.8vo79h.com',
+    base4: 'https://0gqwtu188qp.8vo79h.com',
+    base5: 'https://1cituv188qp.auqiag.com',
+    base6: 'https://pb4wxx188qp.auqiag.com',
+    base7: 'https://1cituv188qp.ayssao.com',
 }
 
 
-export let configAppId = "5"
+export let configAppId = "300002"
 
 
 export const AppConfig = {
@@ -36,41 +36,32 @@ export const AppConfig = {
 }
 
 
-export const MyAppName = '博博乐';
+export const MyAppName = '188棋牌';
 
 export const versionHotFix = '5.23';
 
-export const MyOwnerPlatName= 'uat彩票';
+export const MyOwnerPlatName= '188棋牌';
 
 export const safeguardDomain = [
-    'https://987645ba00a9b0416b254f33d918ed64.oss-cn-shenzhen.aliyuncs.com',
-    'https://c40b6e3d664556ab423d3eebc01ab2fd.oss-cn-shenzhen.aliyuncs.com']
+    'https://7aa58ab0e2988abc6384e8957b567a4c.oss-cn-shenzhen.aliyuncs.com',
+    'https://7aa58ab0e2988abc6384e8957b567a4c.s3-accelerate.amazonaws.com',
+    'https://7aa58ab0e2988abc6384e8957b567a4c.azureedge.net']
 
 
 export const platInfo = {
-    //loginDomain:"https://0365cai.cc",
-    loginDomain: "https://webplatform.psxiaohe.com",
-    //  gameDomain:"https://dk3nuq7v.zhdisi.com",
-    gameDomain: "https://webplatform.psxiaohe.com",
+    loginDomain: "https://1cituv188qp.8vo79h.com",
+    gameDomain: "https://1cituv188qp.8vo79h.com",
 
     downDomain:"https://download.jinkuangjia.com",
 
     zipCheckServer: {
         debug_server: "http://192.168.14.70:8888",
-        release_server: "/game/release/uat"
+        release_server: "/game/release/188qp"
     },
     platId: configAppId,
-    brand: "uat01",
+    brand: "188qp",
     latestNativeVersion:{ios:"2.0",android:"2.0"},//用于强制更新 匹配，与info.plist 还有 gradle.properties. 需要严格一致。否则 会弹窗 强制下载
 }
 
 
-export const affCodeList = {
-    ios: {
-        '2.9.6': 'uat_ios'
-    },
-    android: {
-        '2.10.11': 'uat_android'
-    }
-};
 
