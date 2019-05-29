46f4dab4e0455fcd0c10bc9ef6f8f00254da45b1
taven <abcd1234>
2019-05-29 17:52:36 +0800

    【bug】修复游戏注册问题

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -38,20 +38,16 @@ export const AppConfig = {
 
 export const MyAppName = '博博乐sit';
 
-export const versionHotFix = '5.23';
+export const versionHotFix = 'v6.1';
 
 export const MyOwnerPlatName= 'sit';
 
 // export const safeguardDomain = [
 //     'https://987645ba00a9b0416b254f33d918ed64.oss-cn-shenzhen.aliyuncs.com',
 //     'https://c40b6e3d664556ab423d3eebc01ab2fd.oss-cn-shenzhen.aliyuncs.com']
-
-
-//base64key 格式示例
-// {"d":["https://987645ba00a9b0416b254f33d918ed64.oss-cn-shenzhen.aliyuncs.com","https://c40b6e3d664556ab423d3eebc01ab2fd.oss-cn-shenzhen.aliyuncs.com"]}
-
 //第三方安全域名base64 key
 export const safeguardKey = 'eyJkIjogWyJodHRwczovLzk4NzY0NWJhMDBhOWIwNDE2YjI1NGYzM2Q5MThlZDY0Lm9zcy1jbi1zaGVuemhlbi5hbGl5dW5jcy5jb20iLCAiaHR0cHM6Ly9jNDBiNmUzZDY2NDU1NmFiNDIzZDNlZWJjMDFhYjJmZC5vc3MtY24tc2hlbnpoZW4uYWxpeXVuY3MuY29tIl19'
+
 
 export const platInfo = {
     //loginDomain:"https://0365cai.cc",http://192.168.1.93:8091
