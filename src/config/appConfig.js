00222a662a1a0742f113783b995dbd1ad4a79296
taven <abcd1234>
2019-05-31 17:16:09 +0800

    Merge branches 'develop' and 'release/bbsqp' of https://gitlab.mtgogo.online/APP/BBL_Game into release/bbsqp
    # Conflicts:
    #	android/app/src/main/assets/gamelobby/brand/login_icon.png
    #	src/Page/Route/App.js

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -3,17 +3,16 @@ import React, {
 } from 'react-native';
 
 export const appDomainBase = {
-    base1: 'https://webplatform.psxiaohe.com',
-    base2: 'https://webplatform.psxiaohe.com',
-    base3: 'https://webplatform.psxiaohe.com',
-    base4: 'https://webplatform.psxiaohe.com',
-    base5: 'https://webplatform.psxiaohe.com',
-    base6: 'https://webplatform.psxiaohe.com',
-    base7: 'https://webplatform.psxiaohe.com',
+    base1: 'https://pt2kmby3.kwuesw.com',
+    base2: 'https://pt2kmby3.pnldtz.com',
+    base3: 'https://pt2kmby3.pnldtz.com',
+    base4: 'https://pt2kmby3.pnldtz.com',
+    base5: 'https://pt2kmby3.pnldtz.com',
+    base6: 'https://pt2kmby3.pnldtz.com',
+    base7: 'https://pt2kmby3.weygwy.com',
 }
 
-
-export let configAppId = "5"
+export let configAppId = "280001"
 
 
 export const AppConfig = {
@@ -28,7 +27,7 @@ export const AppConfig = {
         appDomainBase.base5,
         appDomainBase.base6
     ],
-    checkUpdateDomains:[
+     checkUpdateDomains:[
         "https://www.ba2d16.com",
         "https://www.aa2d16.com",
         "https://www.ca2d16.com"
@@ -36,35 +35,35 @@ export const AppConfig = {
 }
 
 
-export const MyAppName = '博博乐';
+export const MyAppName = '博必胜棋牌';
+
+export const MyOwnerPlatName= '博必胜棋牌';
 
 export const versionHotFix = 'v6.1';
 
-export const MyOwnerPlatName= 'uat彩票';
-
-// export const safeguardDomain = [
-//     'https://987645ba00a9b0416b254f33d918ed64.oss-cn-shenzhen.aliyuncs.com',
-//     'https://c40b6e3d664556ab423d3eebc01ab2fd.oss-cn-shenzhen.aliyuncs.com']
-
-
-//base64key 格式示例
-// {"d":["https://987645ba00a9b0416b254f33d918ed64.oss-cn-shenzhen.aliyuncs.com","https://c40b6e3d664556ab423d3eebc01ab2fd.oss-cn-shenzhen.aliyuncs.com"]}
-
 //第三方安全域名base64 key
-export const safeguardKey = 'eyJkIjogWyJodHRwczovLzk4NzY0NWJhMDBhOWIwNDE2YjI1NGYzM2Q5MThlZDY0Lm9zcy1jbi1zaGVuemhlbi5hbGl5dW5jcy5jb20iLCAiaHR0cHM6Ly9jNDBiNmUzZDY2NDU1NmFiNDIzZDNlZWJjMDFhYjJmZC5vc3MtY24tc2hlbnpoZW4uYWxpeXVuY3MuY29tIl19'
+export const safeguardKey = 'IHsiZCI6WyIiaHR0cHM6Ly9hMTE0ZjUzMmU3MDUwMjAyOTViODljNDBiOTAxZTEyNy5vc3MtY24tc2hlbnpoZW4uYWxpeXVuY3MuY29tIiwKICAgICJodHRwczovL2ExMTRmNTMyZTcwNTAyMDI5NWI4OWM0MGI5MDFlMTI3LnMzLWFjY2VsZXJhdGUuYW1hem9uYXdzLmNvbSIsCiAgICAiaHR0cHM6Ly9hMTE0ZjUzMmU3MDUwMjAyOTViODljNDBiOTAxZTEyNy5henVyZWVkZ2UubmV0Il19'
+
+
+
 
 export const platInfo = {
-
-    downDomain:"https://download.jinkuangjia.com",
+    downDomain:"https://download.hkbaoxian188.com",
 
     zipCheckServer: {
         debug_server: "http://192.168.14.70:8888",
-        release_server: "/game/release/uat"
+        release_server: "/game/release/bbsqp"
     },
     platId: configAppId,
-    brand: "uat01",
+    brand: "bbsqp",
     latestNativeVersion:{ios:"2.0",android:"2.0"},//用于强制更新 匹配，与info.plist 还有 gradle.properties. 需要严格一致。否则 会弹窗 强制下载
 }
 
-
-
+export const affCodeList = {
+    ios: {
+        '2.9.6': 'bbsqp_ios'
+    },
+    android: {
+        '2.10.11': 'bbsqp_android'
+    }
+};
