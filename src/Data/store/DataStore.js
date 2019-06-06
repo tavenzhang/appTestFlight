bc30bfc16b0c9996b8b7345687aba5924a2886fa
taven <abcd1234>
2019-06-06 19:10:41 +0800

    【功能】完善加载处理

--- src/Data/store/DataStore.js
+++ src/Data/store/DataStore.js
@@ -293,6 +293,7 @@ export default class DataStore {
             if(!TW_Store.gameUpateStore.isAppDownIng){
                 TW_Store.gameUpateStore.isLoading=false;
                 TW_Store.gameUpateStore.isTempExist=true;
+                TW_Store.gameUpateStore.isNeedUpdate=false;
                 TW_LoaderOnValueJS(TW_Store.bblStore.getWebAction(TW_Store.bblStore.ACT_ENUM.game_loading,{data:{do:"loadFinish"}}));
             }
             if(TW_Store.gameUpateStore.isOldHome){
