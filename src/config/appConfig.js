a0e6753136693419aa60f0bae259ac21687cb7b9
Sam <sam@SamdeMac-mini.local>
2019-06-07 16:08:50 +0800

    autoMerge-releae and replace config

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -37,11 +37,8 @@ export const AppConfig = {
 }
 
 
-export const MyAppName = '博博乐';
-
-export const versionHotFix = 'v6.1';
-
-export const MyOwnerPlatName= 'uat彩票';
+export const MyAppName = '超会赢棋牌';
+export const MyOwnerPlatName= '超会赢棋牌';
 
 export const versionHotFix = 'v6.1';
 
