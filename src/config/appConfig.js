40bdaf26e28b529b139cf0616785ddd8c45bc1aa
Sam <sam@SamdeMac-mini.local>
2019-06-19 17:41:05 +0800

    autoMerge-releae and replace config

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -62,8 +62,9 @@ export const platInfo = {
         release_server: "/game/release/sit"
     },
     platId: configAppId,
-    brand: "uat01",
+    brand: "106",
     latestNativeVersion:{ios:"5.0",android:"5.0"},//用于强制更新 匹配，与info.plist 还有 gradle.properties. 需要严格一致。否则 会弹窗 强制下载
+
 }
 
 
