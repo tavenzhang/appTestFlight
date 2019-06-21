6e0090673211bcda0465b10eb3075185c112c66b
Sam <sam@SamdeMac-mini.local>
2019-06-21 11:13:59 +0800

    autoMerge-releae and replace config

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -55,9 +55,9 @@ export const platInfo = {
         debug_server: "http://192.168.14.70:8888",
         release_server: "/game/release/chyqp",
     },
+    latestNativeVersion:{ios:"2.0",android:"2.0"},//用于强制更新 匹配，与info.plist 还有 gradle.properties. 需要严格一致。否则 会弹窗 强制下载
     platId: configAppId,
-    brand: "uat01",
-    latestNativeVersion:{ios:"5.0",android:"5.0"},//用于强制更新 匹配，与info.plist 还有 gradle.properties. 需要严格一致。否则 会弹窗 强制下载
+    brand: "chyqp",
 }
 
 export const affCodeList = {
