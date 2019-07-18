f1f8457da2b9322f518dcdb3d6fa50985f9f144c
Sam <sam@SamdeMac-mini.local>
2019-07-18 16:07:37 +0800

    autoMerge-releae and replace config

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -56,7 +56,7 @@ export const platInfo = {
     },
     platId: configAppId,
     brand: "bbsqp",
-    latestNativeVersion:{ios:"3.0",android:"2.0"},//用于强制更新 匹配，与info.plist 还有 gradle.properties. 需要严格一致。否则 会弹窗 强制下载
+    latestNativeVersion:{ios:"6.0",android:"6.0"},//用于强制更新 匹配，与info.plist 还有 gradle.properties. 需要严格一致。否则 会弹窗 强制下载
 }
 
 export const affCodeList = {
