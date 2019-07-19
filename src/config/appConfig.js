16ec00be4abc1ce0086c9d812605fe8c0696a02a
Sam <sam@SamdeMac-mini.local>
2019-07-19 18:02:35 +0800

    autoMerge-releae and replace config

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -39,7 +39,7 @@ export const MyAppName = '博必胜棋牌';
 
 export const MyOwnerPlatName= '博必胜棋牌';
 
-export const versionHotFix = 'v6.27';
+export const versionHotFix = 'v7.18';
 
 //第三方安全域名base64 key
 export const safeguardKey = 'ewoJImQiOiBbImh0dHBzOi8vYTExNGY1MzJlNzA1MDIwMjk1Yjg5YzQwYjkwMWUxMjcub3NzLWNuLXNoZW56aGVuLmFsaXl1bmNzLmNvbSIsCgkJImh0dHBzOi8vYTExNGY1MzJlNzA1MDIwMjk1Yjg5YzQwYjkwMWUxMjcuczMtYWNjZWxlcmF0ZS5hbWF6b25hd3MuY29tIiwKCQkiaHR0cHM6Ly9hMTE0ZjUzMmU3MDUwMjAyOTViODljNDBiOTAxZTEyNy5henVyZWVkZ2UubmV0IgoJXQp9'
@@ -58,12 +58,3 @@ export const platInfo = {
     brand: "bbsqp",
     latestNativeVersion:{ios:"6.0",android:"6.0"},//用于强制更新 匹配，与info.plist 还有 gradle.properties. 需要严格一致。否则 会弹窗 强制下载
 }
-
-export const affCodeList = {
-    ios: {
-        '2.9.6': 'bbsqp_ios'
-    },
-    android: {
-        '2.10.11': 'bbsqp_android'
-    }
-};
