c56e6531feaff6b97434526fc385d17add3adefe
Sam <sam@SamdeMac-mini.local>
2019-07-22 11:32:49 +0800

    autoMerge-releae and replace config

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -63,7 +63,7 @@ export const platInfo = {
     },
     platId: configAppId,
     brand: "106",
-    latestNativeVersion:{ios:"7.0",android:"7.0"},//用于强制更新 匹配，与info.plist 还有 gradle.properties. 需要严格一致。否则 会弹窗 强制下载
+    latestNativeVersion:{ios:"8.0",android:"8.0"},//用于强制更新 匹配，与info.plist 还有 gradle.properties. 需要严格一致。否则 会弹窗 强制下载
 
 }
 
