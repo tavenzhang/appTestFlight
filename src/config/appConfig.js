b286a727dbe51360e0921cdad4ea7bd964b15602
Sam <sam@SamdeMac-mini.local>
2019-07-23 09:18:10 +0800

    autoMerge-releae and replace config

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -38,7 +38,7 @@ export const AppConfig = {
 
 export const MyAppName = '博博乐sit';
 
-export const versionHotFix = 'v7.18';
+export const versionHotFix = 'v7.19';
 
 export const MyOwnerPlatName= 'sit';
 
